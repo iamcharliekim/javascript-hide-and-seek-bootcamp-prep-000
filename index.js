fb8a49c5a7c7c212d048a2b20656e3e57bb60cb6
Charles Kim <cwkim3@gmail.com>
2018-07-28 22:48:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function getFirstSelector(selector){
+  var output = document.querySelector(selector);
+  return output;
+}
 
+function nestedTarget(){
+  var output = document.querySelector("#nested.target");
+  return output;
+}
