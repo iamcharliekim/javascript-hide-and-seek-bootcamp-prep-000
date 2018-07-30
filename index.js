3b0beac5bf20ae3787765478ae23e09795a6cdda
Charles Kim <cwkim3@gmail.com>
2018-07-30 18:21:21 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function nestedTarget(){
   var output = document.querySelector("#nested.target");
   return output;
 }
+
+function increaseRankBy(n){
+  var output = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < output.length; i++){
+    output[i].innerHTML = parseInt(output[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var output = document.querySelector('#grand-node');
+  var nextChild = output.children[0];
+  while (nextChild){
+    output = nextChild;
+    nextChild = nextChild.children[0];
+  }  
+  return output;
+}
