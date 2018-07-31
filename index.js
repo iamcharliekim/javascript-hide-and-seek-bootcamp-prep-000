d7a382f56dc58412b86f1d07deebed01360320fe
Charles Kim <cwkim3@gmail.com>
2018-07-31 23:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,25 +2,3 @@ function getFirstSelector(selector){
   var output = document.querySelector(selector);
   return output;
 }
-
-function nestedTarget(){
-  var output = document.querySelector("#nested.target");
-  return output;
-}
-
-function increaseRankBy(n){
-  var output = document.querySelectorAll('ul.ranked-list li');
-  for (let i = 0; i < output.length; i++){
-    output[i].innerHTML = parseInt(output[i].innerHTML) + n;
-  }
-}
-
-function deepestChild(){
-  var output = document.querySelector('#grand-node');
-  var nextChild = output.children[0];
-  while (nextChild){
-    output = nextChild;
-    nextChild = nextChild.children[0];
-  }  
-  return output;
-}
