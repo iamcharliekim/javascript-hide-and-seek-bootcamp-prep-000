8c16c8ee75608920a983598e69d7e72a1fad5dcc
Charles Kim <cwkim3@gmail.com>
2018-07-31 23:18:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   var output = document.querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < output.length; i++){
-    console.log(output[i].innerHTML)
+    output[i].innerHTML
 }
 }
